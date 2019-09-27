93d7fd18dec04a7adea3b0450ba5ce00d874de1f
Prbvcelari <bvcelari@gmail.com>
2019-09-27 15:57:07 +0200

    Embedded the bash

--- vars/ESHelper.groovy
+++ vars/ESHelper.groovy
@@ -1,4 +1,23 @@
 def call(Map parameters) {
-    sh "echo this a test ${parameters.env}"
-}
+    sh 'Sending informatino to ES:'
 
+    sh """
+    cat > message.json <<EOF
+    {
+    "enviroment": "${ESDataSet.env}",
+    "build_number": "${ESDataSet.build_number}",
+    "build_tag": "${ESDataSet.build_tag}",
+    "job_base_name": "${ESDataSet.job_base_name}",
+    "job_name": "${ESDataSet.job_name}",
+    "node_name": "${ESDataSet.node_name}",
+    "node_labels": "${ESDataSet.node_labels}",
+    "status": "${ESDataSet.status}",
+    "date": "${ESDataSet.date}"
+    }
+    EOF
+    
+    cat message.json
+    curl -u USERNAME:PASSWORD -XPUT 'localhost:9200/custom_builds/external/1?pretty' -d '@message.json'
+    """
+
+}
