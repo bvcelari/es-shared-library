e3e9e8b17c5c03eb0a6f623a3980d8b1a71a3a90
Juan Perez <bvcelari@gmail.com>
2020-12-15 12:43:15 +0100

    Update ESHelper.groovy

--- vars/ESHelper.groovy
+++ vars/ESHelper.groovy
@@ -1,26 +1,4 @@
-def call(Map ESDataSet) {
-    sh 'echo "Sending information to ES... "'
-
-    sh """
-    cat > message.json <<EOF
-    {
-    "enviroment": "${ESDataSet.env}",
-    "build_number": "${ESDataSet.build_number}",
-    "build_tag": "${ESDataSet.build_tag}",
-    "job_base_name": "${ESDataSet.job_base_name}",
-    "job_name": "${ESDataSet.job_name}",
-    "node_name": "${ESDataSet.node_name}",
-    "node_labels": "${ESDataSet.node_labels}",
-    "status": "${ESDataSet.status}",
-    "date": "${ESDataSet.date}"
-    }
-    EOF
-    """
-    sh """
-    echo "   generated message..."
-    cat message.json
-    
-    curl -u USERNAME:PASSWORD -XPUT 'localhost:9200/custom_builds/external/1?pretty' -d '@message.json'
-    """
+def call(String mystring) {
+    sh 'echo "Sending information to ES... "${mystring}'
 
 }
