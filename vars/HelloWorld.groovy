c921a1ef4fbfa812a517d013428afa95e86fa6e1
Juan Perez <bvcelari@gmail.com>
2020-12-17 10:55:42 +0100

    Update HelloWorld.groovy

--- vars/HelloWorld.groovy
+++ vars/HelloWorld.groovy
@@ -1,9 +1,1 @@
-def call(String mydata) {
-  println "Hello World!!"
-  println mydata
-  File file = new File("/tmp/out.txt")
-  file.write "First line\n"
-  file << "Second line\n"
-  println file.text
-
-}
+println "Hello World!!"
