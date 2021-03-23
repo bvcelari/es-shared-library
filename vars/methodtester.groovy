8804580e23089ee540f700c45c06a97b4cf93acf
Juan Perez <bvcelari@gmail.com>
2021-03-23 13:04:01 +0100

    Update methodtester.groovy

--- vars/methodtester.groovy
+++ vars/methodtester.groovy
@@ -1,7 +1,7 @@
 def call() {
     echo "Starting session..."
     sh 'echo "gathering info "'
-    echo "${params.myparam_one}
+    echo "${params.myparam_one}"
     echo "generated message..."
     
 }
