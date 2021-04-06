c5ffad58085e263da2856827d8efa547a5243fbc
Juan Perez <bvcelari@gmail.com>
2021-04-06 14:04:33 +0200

    Update methodtester.groovy

--- vars/methodtester.groovy
+++ vars/methodtester.groovy
@@ -3,8 +3,8 @@ def call() {
     sh 'echo "gathering info "'
     echo "${params.myparam_one}"
     echo "generated message..."
-    echo 'Param one inside the job like: ${myparam_one}'
-    echo "Param one inside the job like: ${myparam_one}"
+    echo "Hello new1  ${params.myparam_one}"
+    echo "Hello new2 ${myparam_one}"
 
     
 }
