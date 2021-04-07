36610990add8cf9149a445bb834d090329643a86
Juan Perez <bvcelari@gmail.com>
2021-04-07 08:28:49 +0200

    Update methodtester.groovy

--- vars/methodtester.groovy
+++ vars/methodtester.groovy
@@ -1,10 +1,9 @@
 def call() {
     echo "Starting session..."
     sh 'echo "gathering info "'
-    echo "${params.myparam_one}"
+    echo "Hello new1 ${myparam_one}"
+    echo "Hello new2 ${myparam_two}"
     echo "generated message..."
-    echo "Hello new1  ${params.myparam_one}"
-    echo "Hello new2 ${myparam_one}"
 
     
 }
